993a0bf95a53730629a6fb03ce1e51c49895f033
Andras Slemmer <andras.slemmer@r3cev.com>
2017-08-10 13:54:38 +0100

    Move useTestClock conditional to Node constructor

--- test-utils/src/main/kotlin/net/corda/testing/node/NodeBasedTest.kt
+++ test-utils/src/main/kotlin/net/corda/testing/node/NodeBasedTest.kt
@@ -161,7 +161,7 @@ abstract class NodeBasedTest : TestDependencyInjectionBase() {
 
         val parsedConfig = config.parseAs<FullNodeConfiguration>()
         val node = Node(parsedConfig, parsedConfig.calculateServices(), MOCK_VERSION_INFO.copy(platformVersion = platformVersion),
-                if (parsedConfig.useTestClock) TestClock() else NodeClock(), initialiseSerialization = false)
+                initialiseSerialization = false)
         node.start()
         nodes += node
         thread(name = legalName.commonName) {
